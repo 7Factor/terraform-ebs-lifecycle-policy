10faffb564904b1d23e317635f0d236091eee64a
chris-malloy <chrismalloymusic@gmail.com>
2020-06-10 13:29:20 -0400

    Testing iam.

---
+++ integration/spec/main_spec.rb
@@ -0,0 +1,36 @@
+
+require 'awspec'
+require 'hcl/checker'
+
+TFVARS = HCL::Checker.parse(File.open('testing.tfvars').read())
+TFOUTPUTS = eval(ENV['KITCHEN_KITCHEN_TERRAFORM_OUTPUTS'])
+
+describe iam_role('ebs-lifecycle-policy-role') do
+  it { should exist }
+  it do
+    should have_inline_policy('ebs-lifecycle-policy').policy_document(<<-'DOC')
+{
+   "Version": "2012-10-17",
+   "Statement": [
+      {
+         "Effect": "Allow",
+         "Action": [
+            "ec2:CreateSnapshot",
+            "ec2:DeleteSnapshot",
+            "ec2:DescribeVolumes",
+            "ec2:DescribeSnapshots"
+         ],
+         "Resource": "*"
+      },
+      {
+         "Effect": "Allow",
+         "Action": [
+            "ec2:CreateTags"
+         ],
+         "Resource": "arn:aws:ec2:*::snapshot/*"
+      }
+   ]
+}
+    DOC
+  end
+end
